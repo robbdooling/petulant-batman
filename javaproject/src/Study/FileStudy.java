4a3bd4c36bdc8f72b523d39fbf9af45acfb251b6
Robert Lowe <rob@lowefrequency.com>
2013-09-28 18:38:10 +0000

    Updated the Study interface to include basic functionality

--- javaproject/src/Study/FileStudy.java
+++ javaproject/src/Study/FileStudy.java
@@ -2,4 +2,22 @@ package Study;
 
 public class FileStudy implements Study {
 
+	@Override
+	public int getIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setIndex(int newIndex) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- javaproject/src/Study/Study.java
+++ javaproject/src/Study/Study.java
@@ -1,5 +1,7 @@
 package Study;
 
 public interface Study {
-
+	public int getIndex();
+	public void setIndex(int newIndex);
+	public String getLocation();
 }
