4cd34929541a6f927d0a01574f5c7897b2a25d40
BubbleNet <raquasa123@gmail.com>
2013-09-29 19:06:04 -0400

    Added View package which contains UI classes

---
+++ javaproject/src/View/ViewTester.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class ViewTester {
+
+	public static void main(String[] args) {
+		
+		Frame pgm = new Frame();
+		pgm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pgm.setSize(960,720);
+		pgm.setVisible(true);
+		
+	}
+
+}
