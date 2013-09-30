1d6264a732172ecf7a72e3fe84043a2715915a84
Robert Lowe <rob@lowefrequency.com>
2013-09-30 01:02:05 +0000

    Added example files

--- javaproject/src/Study/StudyBuilder.java
+++ javaproject/src/Study/StudyBuilder.java
@@ -4,4 +4,7 @@ public class StudyBuilder {
 	void setSource(int locationType){
 		//sets the source of the study, only file is available at this time
 	}
+	void setFileLocation(String fileLocation){
+		//sets the location of the folder containing the images
+	}
 }
