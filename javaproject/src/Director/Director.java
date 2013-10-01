ae5a969b6361e3f21ae8ba56e1be71a7ed3774b1
Robert Lowe <rob@lowefrequency.com>
2013-10-01 11:43:13 +0000

    Added basic director functionality stuff

---
+++ javaproject/src/Director/Director.java
@@ -0,0 +1,16 @@
+package Director;
+
+import Study.Study;
+//TODO implement everything
+public class Director {
+	static final Director D = new Director();
+	public static Director D(){
+		return D;
+	}
+	public Study getStudy(){
+		return null;	
+	}
+	public void setStudy(Study newStudy){
+		
+	}
+}
