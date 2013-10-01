999cfb8f191911eb69509c349f8819e5bfb9d821
Robert Lowe <rob@lowefrequency.com>
2013-10-01 12:05:04 +0000

    Added State subsystem

---
+++ javaproject/src/State/FourState.java
@@ -0,0 +1,11 @@
+package State;
+
+public class FourState implements State {
+
+	@Override
+	public void images() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ javaproject/src/State/OneState.java
@@ -0,0 +1,11 @@
+package State;
+
+public class OneState implements State {
+
+	@Override
+	public void images() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
