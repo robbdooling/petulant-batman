f84bfe974ce783def49d4627f5a1c4645f0a51b7
Robert Lowe <rob@lowefrequency.com>
2013-10-01 11:49:54 +0000

    Began implementing reindexcommand

--- javaproject/src/Command/ReindexCommand.java
+++ javaproject/src/Command/ReindexCommand.java
@@ -3,11 +3,16 @@
  */
 package Command;
 
+import Director.Director;
+import Study.Study;
+
 public abstract class ReindexCommand implements Command {
 
 	@Override
 	public void execute() {
-				
+		Study currentStudy = Director.D().getStudy();
+		int currentIndex = currentStudy.getIndex();
+		
 	}
 	abstract public int direction();
 	//This determines the direction the command goes in. -1 is to the left, 1 is to the right
