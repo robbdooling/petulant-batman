7e63caed6fb07ac9fd2f166cf4c516665402c7db
Robert Lowe <rob@lowefrequency.com>
2013-10-03 03:35:39 +0000

    Added javadocs to director

--- javaproject/src/Director/Director.java
+++ javaproject/src/Director/Director.java
@@ -3,29 +3,54 @@ package Director;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import State.StateHolder;
 import Study.NoValidStudiesFoundException;
 import Study.Study;
 import Study.StudyBuilder;
 import Study.StudyBuilder.StudyType;
+/**
+ * Simplifys talking to and stores the currently opened Study
+ * @author rob
+ *
+ */
 public class Director {
 	private static List<Study> availStudies = null;
 	private static String root = "";
 	private static Study study = null;
+	/**
+	 * Gets the current study
+	 * @return The current study loaded in
+	 */
 	public static Study getStudy(){
 		return study;
 	}
+	/**
+	 * Sets the study to a new value
+	 * @param newStudy the new study to replace the current one with
+	 */
 	public static void setStudy(Study newStudy){
 		study = newStudy;
 	}
+	/**
+	 * Returns a list of images to load into the gui based on the currentstate
+	 * @return the list of images to load
+	 */
 	public static List<String> getImages(){
 		return study.getImgAddresses().subList(study.getIndex(), (study.getIndex() + StateHolder.images() - 1));
 		
 	}
+	/**
+	 * Set the root search directory to a different path
+	 * @param newRoot new Root path
+	 */
 	public static void setRoot(String newRoot){
 		root = newRoot;
 	}
+	/**
+	 * Gets all of the valid directories that are studies
+	 * @return All valid directories 
+	 * @throws NoValidStudiesFoundException
+	 */
 	public static List<String> getAvailStudies() throws NoValidStudiesFoundException{
 		availStudies =  Arrays.asList(StudyBuilder.getAvailableStudies(root, StudyType.local));
 		List<String> stringStudies = new ArrayList<String>();
@@ -34,9 +59,17 @@ public class Director {
 		}
 		return stringStudies;
 	}
+	/**
+	 * Chooses a study based on an index of the getAvailStudies
+	 * @param Index The index of the chosen study
+	 */
 	public static void choseStudy(int Index){
 		study = availStudies.get(Index);
 	}
+	/**
+	 * Indicates if there is anything to the "left"
+	 * @return boolean that indicates whether it's possible to move to the left
+	 */
 	public static boolean isLeft(){
 		if(study == null){
 			return false;
@@ -45,6 +78,10 @@ public class Director {
 		int step = StateHolder.images();
 		return ((currentIndex - step) < 0);
 	}
+	/**
+	 * Indicates if there is anything to the "right"
+	 * @return boolean that indicates whether it's possible to move to the right
+	 */
 	public static boolean isRight(){
 		if(study == null){
 			return false;
